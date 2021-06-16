9a9ea50fcc53f0424b62191bcbc6c4dff96e68e7
ArthurRosso <arthurderosso@gmail.com>
2021-06-15 21:47:40 -0300

    add heroku credentials and short paths

--- config.php
+++ config.php
@@ -6,11 +6,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 /* Attempt to connect to PostgreSQL database */
-$host = "127.0.0.1";
+$host = "ec2-18-215-111-67.compute-1.amazonaws.com";
 $port = "5432";
-$dbname = "hidro-esp";
-$user = "arthur";
-$passwd = "";
+$dbname = "daglc97fpn9gbh";
+$user = "fzfulpxratkqfd";
+$passwd = "484482d6275bd52888700c54b68ed3009a0a91a3d88134c2723e9d0eed6d7391";
 $con_string = "host=$host port=$port dbname=$dbname user=$user password=$passwd sslmode=require";
 $link = pg_connect($con_string);
  
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 if(isset($_COOKIE['mac'])){
-	header("Location: /hidro-esp-srv/read.php"); /* Redirect browser */
+	header("Location: /read.php"); /* Redirect browser */
 
     /* Make sure that code below does not get executed when we redirect. */
     exit;
 }else{
-	header("Location: /hidro-esp-srv/login.html"); /* Redirect browser */
+	header("Location: /login.html"); /* Redirect browser */
 
     /* Make sure that code below does not get executed when we redirect. */
     exit;
